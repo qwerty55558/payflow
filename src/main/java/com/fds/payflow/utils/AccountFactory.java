b45f21e14365f61de3da44e2abcfeb923d634a8f
Admin <amajang2012@gmail.com>
2025-10-20 10:51:45 +0900

    FIN-3 MVC 패턴 구현

--- src/main/java/com/fds/payflow/utils/AccountFactory.java
+++ src/main/java/com/fds/payflow/utils/AccountFactory.java
@@ -13,6 +13,10 @@ import java.util.UUID;
 @Slf4j
 public class AccountFactory {
     public Account createAccount(Member member){
-        return new Account(UUID.randomUUID().toString(), member.getUserId() + "의 통장", 100000L, member);
+        return new Account(generateAccountNumber(), member.getUserId() + "의 통장", 100000L, member);
+    }
+
+    private String generateAccountNumber(){
+        return UUID.randomUUID().toString().replace("-", "").substring(0,6);
     }
 }
